a412e54e31715950c9b9d023ac9bbea996090dd3
ZagkasD <72259822+ZagkasD@users.noreply.github.com>
2022-03-06 13:55:20 +0200

    Updates to compilers_1_v1.1
    Moved global variables below classes
    Added return and char limit to automaton state 1 and 2
    Fixed bug in automaton state 1
    Added backtracking to state 1
    State 1 is complete
    Added backtracking to state 2
    State 2 is complete
    Added return to state 4, added backtracking
    State 4 is complete
    Added return for symbols, delimiters, grouping symbols
    State 5 is complete
    Updated end of file return
    Return for end of program
    Completed comments
    
    =============================
    Lex Analyzer is compete
    =============================

--- compiler_1_v1.1.py
+++ compiler_1_v1.1.py
@@ -1,46 +1,62 @@
-from pickle import NONE
+# Zagkas Dimosthenis 4359 cse84359
+# Andreou Aggelos    4628 cse84628
 import sys
-
-# Global Variables
-line_number = 1
-input_file = NONE
 
 class Token:
 	# properties: family, recognized_string, line_numbers
-	def __init__(self, family, recognized_string, line_number):
-		self.family = family
+	def __init__(self, family_type, recognized_string, line_number):
+		self.family_type = family_type
 		self.recognized_string = recognized_string
 		self.line_number = line_number
 
-	def set_family(self, family_type):
+	def set_family_type(self, family_type):
 		self.family_type = family_type
 
 class Family: 
-	def __init__(self):
-		pass
-	
+
+	number = 'number'	
+	keyword = 'keyword'		# if,while...
+	id = 'id'			# identifiers
+	addOperator = 'addOperator'	# +,-
+	mulOperator = 'mulOperator'	# *,/
+	relOperator = 'relOperator'	# ==,>=,<,<>
+	assignment = 'assignmet'	# :=
+	delimiter = 'delimiter'	# ,,.,;
+	groupSymbol = 'groupSymbol'	# (,),{,},[,]
+	end_of_file = 9 
+	'''
 	number = 0	
-	keyword = 1
-	id = 2
-	addOperator = 3	# +.-
+	keyword = 1		# if,while...
+	id = 2			# identifiers
+	addOperator = 3	# +,-
 	mulOperator = 4	# *,/
 	relOperator = 5	# ==,>=,<,<>
 	assignment = 6	# :=
 	delimiter = 7	# ,,.,;
 	groupSymbol = 8	# (,),{,},[,]
+	end_of_file = 9 
+	'''
 
 keywords=['program','declare','if','else','while','switchcase','forcase','incase','case','default','not','and','or','function',
 		  'procedure','call','return','in', 'inout','input', 'print']
 
+# Global Variables
+line_number = 1
+token = Token(None,None,None)
+
 # argv[1] is the first console argument, aka file name
 def error( error_msg,line_number):
-	print("Error in File",sys.argv[1], 'line', line_number, error_msg)
+	#print("Error in file:",sys.argv[1],'||','line:', line_number,'||', error_msg)
+	print("Error in file: testing.txt",'||','line:', line_number,'||', error_msg)
 	exit(1)
 
+#========================
+#      Lex Analyzer
+#========================
 
 def lex(input_file):
 	global line_number
-	while True:
+	while True: 
 	
 		# read first character
 		char = input_file.read(1)
@@ -60,12 +76,22 @@ def lex(input_file):
 		elif char.isalpha():
 			token_string = char
 			while char.isalpha() or char.isdigit():
+				if len(token_string) > 30:
+					error('Length of word can not exceed 30.',line_number)
 				char = input_file.read(1)
-				token_string += char
-				# TODO store line number
-				# TODO Backtracking
-			#TODO KEYWORDS
-		
+				# Need a second check for char
+				# Ex. input: i<3
+				# char = i, token_string = i, next read -> char = <
+				# without this second if statement, token_string = i<
+				if char.isalpha() or char.isdigit():
+					token_string += char
+			# Backtracking
+			input_file.seek(input_file.tell() - 1)
+			if token_string in keywords:
+				return Token(Family.keyword,token_string,line_number)
+			else:
+				return Token(Family.id,token_string,line_number)
+
 		# automaton state 2
 		# digits
 		elif char.isdigit():
@@ -74,27 +100,12 @@ def lex(input_file):
 				char = input_file.read(1)
 				if char.isalpha():
 					error("Arithmetic constants can not contain letters.", line_number)
-				token_string += char
-				# TODO add char limit
-				# TODO Backtracking
+				if int(token_string) > (2**32 -1) or int(token_string) < (-(2**32 -1)):
+					error('Arithmetic constants must be within -(2^32 - 1) and (2^32 - 1)')
+				if char.isdigit():
+					token_string += char
+			input_file.seek(input_file.tell() - 1)
 			return Token(Family.number, token_string, line_number)
-
-		# Symbols +,-,*,/,=
-		elif char == '+':
-				# TODO return token
-				pass
-		elif char == '-':
-			# TODO return token
-			pass
-		elif char == '*':
-			# TODO return token
-			pass
-		elif char == '/':
-			# TODO return token
-			pass
-		elif char == '+':
-			# TODO return token
-			pass
 
 		# automaton state 3
 		# char = '<'
@@ -103,28 +114,25 @@ def lex(input_file):
 			char = input_file.read(1)
 			if char == '=':
 				token_string += char
-				# TODO return token <=
-				pass
+				return Token(Family.relOperator, token_string, line_number)
 			elif char == '>':
-				# TODO return token <>
-				pass
-
+				token_string += char
+				return Token(Family.relOperator, token_string, line_number)
 			else:
-				# TODO return token <
-				pass
+				input_file.seek(input_file.tell() - 1)
+				return Token(Family.relOperator, token_string, line_number)
 
 		# automaton state 4
 		# char = '>'
-		elif char == '<':
+		elif char == '>':
 			token_string = char
 			char = input_file.read(1)
 			if char == '=':
 				token_string += char
-				# TODO return token <=
-				pass
+				return Token(Family.relOperator, token_string, line_number)
 			else:
-				# TODO return token >
-				pass
+				input_file.seek(input_file.tell() - 1)
+				return Token(Family.relOperator, token_string, line_number)
 
 		# automaton state 5
 		# char = ':'
@@ -133,39 +141,63 @@ def lex(input_file):
 			char = input_file.read(1)
 			if char == '=':
 				token_string += char
-				# TODO return token <=
-				pass
-			else:
-				# TODO return token >
-				error(": must be followed by =") # TODO make this nice
+				return Token(Family.assignment, token_string, line_number)
+			else:				 
+				error(': must be followed by =', line_number)			
+		
+		# Symbols +,-,*,/,=
+		elif char == '+':
+			token_string  = char
+			return Token(Family.addOperator, token_string, line_number)
+			
+		elif char == '-':
+			token_string  = char
+			return Token(Family.addOperator, token_string, line_number)
+
+		elif char == '*':
+			token_string  = char
+			return Token(Family.mulOperator, token_string, line_number)
+			
+		elif char == '/':
+			token_string  = char
+			return Token(Family.mulOperator, token_string, line_number)
 
 		# Delimeters , ; 
 		elif char == ',':
-			token_string = char	# TODO teturn token
+			token_string = char	
+			return Token(Family.delimiter, token_string, line_number)
 		elif char == ';':
-			token_string = char # TODO teturn token
-
+			token_string = char 
+			return Token(Family.delimiter, token_string, line_number)
 		#EOF
 		elif char == '':
-			pass	# TODO return token
+			token_string = char
+			return Token(Family.end_of_file, token_string, line_number)
 		
 		# Grouping Symbols
 		elif char == '(':
 			token_string = char
+			return Token(Family.groupSymbol, token_string, line_number)
 		elif char == ')':
 			token_string = char
+			return Token(Family.groupSymbol, token_string, line_number)
 		elif char == '{':
 			token_string = char
+			return Token(Family.groupSymbol, token_string, line_number)
 		elif char == '}':
 			token_string = char
+			return Token(Family.groupSymbol, token_string, line_number)
 		elif char == '[':
 			token_string = char
+			return Token(Family.groupSymbol, token_string, line_number)
 		elif char == ']':
 			token_string = char
+			return Token(Family.groupSymbol, token_string, line_number)
 		
 		# End of program
 		elif char == '.':
 			token_string = char
+			return Token(Family.delimiter, token_string, line_number)
 		
 		# automaton state 6
 		# Comments
@@ -173,23 +205,26 @@ def lex(input_file):
 			char = input_file.read(1)
 			while char !='#':
 				char = input_file.read(1)
-				#todo line number ++ if /n
+				# Counting lines in comments
+				if char == '\n':
+					line_number += 1
+				# If we reach eof while in comments, error
 				if char=='': 
-					error() # TODO line numbers
+					error('Reached eof without closing comments',line_number)
 		else: 
-			error() # TODO line numbers
+			error('Illegal character.',line_number)
 
 
-#def main():
-def main(input_file):
+def main():
+#def main(input_file):
 
-	input_file = open(input_file,'r')
-	#input_file = open('testing.txt','r')
+	#input_file = open(input_file,'r')
+	input_file = open('testing.txt','r')
+	while True:
+		token = lex(input_file)
+		print(token.recognized_string, 'family:', token.family_type, 'line:', token.line_number)
 
-	token = lex(input_file)
 
-	input_file.close	
-
-main(sys.argv[1])
-#main()
+#main(sys.argv[1])
+main()
 
